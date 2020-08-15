d8aa05f019cbc05d521cf3de6fc9434346efd44b
alejandropareds <69061334+alejandropareds@users.noreply.github.com>
2020-08-14 20:52:52 -0500

    Initial commit

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,62 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+
+        var push = PushNotification.init({
+            android:{
+
+            },ios:{
+                alert:"true",
+                badge:true,
+                sound:'false'
+            }
+      });
+
+      push.on('registration',function(data){
+
+        console.log(data.registrationId);
+        console.log(data.registrationType);
+
+      });
+
+      push.on('notification',function(data){ 
+
+        console.log(data.message);
+        console.log(data.title);
+        console.log(data.count);
+        console.log(data.sound);
+        console.log(data.image);
+        console.log(data.additionalData);
+
+      });
+
+
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
